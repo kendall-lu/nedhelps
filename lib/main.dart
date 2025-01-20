7fe6cc5df4cc82ba1aa10765dda5a81af6fb7a68
Kendall <kendall@chekspend.com>
2025-01-20 14:37:00 -0800

    implement: slider, refactor how to update state

--- lib/main.dart
+++ lib/main.dart
@@ -27,18 +27,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -60,6 +49,10 @@ Future<Response> fetchData() async {
       'https://gist.githubusercontent.com/motgi/8fc373cbfccee534c820875ba20ae7b5/raw/7143758ff2caa773e651dc3576de57cc829339c0/config.json'));
 }
 
+String fromDollars(String dollars) {
+  return dollars.replaceAll(RegExp(r'[^\w\s]+'), '');
+}
+
 class MyComponent extends StatefulWidget {
   const MyComponent({super.key});
 
@@ -67,8 +60,21 @@ class MyComponent extends StatefulWidget {
   State<MyComponent> createState() => _MyComponentState();
 }
 
+// ignore: constant_identifier_names - using JSON values to key into handleUpdate
+enum KnownKey { revenue_amount }
+
 class _MyComponentState extends State<MyComponent> {
   Metadata? revenue;
+  Metadata? fundingAmount;
+
+  Map<KnownKey, dynamic> knownKeyToValue = {KnownKey.revenue_amount: 0};
+
+  void handleUpdate({required KnownKey key, required dynamic value}) {
+    setState(() {
+      knownKeyToValue[key] = value;
+    });
+  }
+
   @override
   void initState() {
     fetchData().then((res) {
@@ -77,8 +83,15 @@ class _MyComponentState extends State<MyComponent> {
         decodedJson.forEach((json) {
           var metadata = Metadata.fromJson(json);
           setState(() {
-            if (metadata.name == 'revenue_amount') {
-              revenue = metadata;
+            switch (metadata.name) {
+              case 'revenue_amount':
+                revenue = metadata;
+                handleUpdate(
+                    key: KnownKey.revenue_amount,
+                    value: fromDollars(metadata.placeholder));
+                break;
+              case 'funding_amount':
+                fundingAmount = metadata;
             }
           });
         });
@@ -93,23 +106,21 @@ class _MyComponentState extends State<MyComponent> {
       decoration: BoxDecoration(
         border: Border.all(color: Colors.blue),
       ),
-      // width: 750,
-      // height: 500,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
             child: FinancingOptions(
               revenue: revenue,
+              fundingAmount: fundingAmount,
+              knownKeyToValue: knownKeyToValue,
+              handleUpdate: handleUpdate,
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(color: Colors.blue),
           ),
           Expanded(
             child: Placeholder(),
           ),
+          SizedBox.shrink()
         ],
       ),
     );
@@ -118,9 +129,17 @@ class _MyComponentState extends State<MyComponent> {
 
 class FinancingOptions extends StatefulWidget {
   final Metadata? revenue;
+  final Metadata? fundingAmount;
+  final Map<KnownKey, dynamic> knownKeyToValue;
+  final void Function({required KnownKey key, required dynamic value})
+      handleUpdate;
+
   const FinancingOptions({
     super.key,
-    this.revenue,
+    required this.revenue,
+    required this.fundingAmount,
+    required this.knownKeyToValue,
+    required this.handleUpdate,
   });
 
   @override
@@ -129,7 +148,8 @@ class FinancingOptions extends StatefulWidget {
 
 class _FinancingOptionsState extends State<FinancingOptions> {
   TextEditingController? financingOptionsController;
-  int? initialValue;
+  double? initialValue;
+
   @override
   void initState() {
     var revenue = widget.revenue;
@@ -141,11 +161,7 @@ class _FinancingOptionsState extends State<FinancingOptions> {
 
   @override
   Widget build(BuildContext context) {
-    Metadata? revenue = widget.revenue;
-    String initialValue = revenue != null
-        ? int.parse(revenue.placeholder.replaceAll(RegExp(r'[^\w\s]+'), ''))
-            .toString()
-        : '';
+    var revenueAmount = widget.knownKeyToValue[KnownKey.revenue_amount];
     return Card(
         margin: EdgeInsets.zero,
         child: SizedBox.expand(
@@ -156,18 +172,96 @@ class _FinancingOptionsState extends State<FinancingOptions> {
               'Financing options',
               style: TextStyle(fontSize: 30),
             ),
-            if (initialValue.isNotEmpty)
-              TextFormField(
-                autovalidateMode: AutovalidateMode.always,
-                keyboardType: TextInputType.number,
-                controller: financingOptionsController,
-                initialValue: initialValue,
-                decoration: InputDecoration(
-                  prefixIcon: Text("\$"),
-                  hintText: revenue?.label ?? '',
-                ),
-              ),
+            Text(widget.revenue?.label ?? ''),
+            TextFormField(
+              key: Key(revenueAmount.toString()),
+              autovalidateMode: AutovalidateMode.always,
+              keyboardType: TextInputType.number,
+              controller: financingOptionsController,
+              initialValue: revenueAmount,
+              onChanged: (val) {
+                var parsedVal = int.tryParse(val);
+                if (parsedVal != null && parsedVal > 0) {
+                  widget.handleUpdate(
+                      key: KnownKey.revenue_amount,
+                      value: parsedVal.toString());
+                }
+              },
+            ),
+            RevenueAmount(
+                fundingAmount: widget.fundingAmount,
+                knownKeyToValue: widget.knownKeyToValue,
+                key: Key(initialValue.toString()))
           ],
         )));
+  }
+}
+
+class RevenueAmount extends StatefulWidget {
+  final Metadata? fundingAmount;
+  final Map<KnownKey, dynamic> knownKeyToValue;
+  const RevenueAmount({
+    super.key,
+    required this.fundingAmount,
+    required this.knownKeyToValue,
+  });
+
+  @override
+  State<RevenueAmount> createState() => _RevenueAmountState();
+}
+
+class _RevenueAmountState extends State<RevenueAmount> {
+  double fundingAmount = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int revenueAmount =
+        int.parse(widget.knownKeyToValue[KnownKey.revenue_amount]);
+
+    double maxRevenue = (revenueAmount / 3);
+    return Row(children: [
+      Expanded(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(widget.fundingAmount?.label ?? ''),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [Text('0'), Text('$maxRevenue')],
+          ),
+          Slider(
+            min: 0,
+            value: fundingAmount.toDouble(),
+            max: maxRevenue,
+            onChanged: (val) {
+              setState(() {
+                fundingAmount = val;
+              });
+            },
+          ),
+        ],
+      )),
+      SizedBox(
+          width: 100,
+          height: 50,
+          child: TextFormField(
+            keyboardType: TextInputType.number,
+            key: Key(fundingAmount.toString()),
+            initialValue: fundingAmount.toString(),
+            onChanged: (val) {
+              // TODO: Fix bug..
+              var parsedVal = int.tryParse(val);
+              if (parsedVal != null && parsedVal <= maxRevenue) {
+                setState(() {
+                  fundingAmount = parsedVal.toDouble();
+                });
+              }
+            },
+          ))
+    ]);
   }
 }
